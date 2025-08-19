8a1b145daadd46d742dfca65ea1e78e83881bc94
diki <muliaandiki@gmaiil.com>
2025-08-20 02:02:29 +0700

    feat: initial

---
+++ app/(pages)/(publik)/dashboard/_container/dashboard.tsx
@@ -0,0 +1,13 @@
+import dynamic from "next/dynamic";
+
+const DashboardContainer = () => {
+  return (
+    <div className="h-full w-full">
+      <div className="flex justify-center items-center">
+        <h1>Init Setup</h1>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardContainer;
